96296290d53435c16b354fcfb7e8fb665cbecf35
Marco Rodrigues <84645887+masrodrigues@users.noreply.github.com>
2024-04-20 19:15:38 -0300

    Criação do banco

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 # Create your views here.
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+        
+        if senha != confirmar_senha:
+            print('erro2') #retirar print
+            return redirect('/usuarios/cadastro')
+        
+
+        if len(senha) < 6:
+            print('erro3') #retirar print
+            return redirect('/usuarios/cadastro')
+        
+        users = User.objects.filter(username=username)
+        
+        if users.exists():
+            print('Erro1"=') #retirar print
+            return redirect('/usuarios/cadastro')
+        
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=senha
+        ) 
+    return HttpResponse(f'Usuario criado com sucesso.')
