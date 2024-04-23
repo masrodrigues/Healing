900d299443e911a96abdd64f6ee1a61f2cd72cd7
Marco Rodrigues <84645887+masrodrigues@users.noreply.github.com>
2024-04-22 23:04:36 -0300

    Criando página de login

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages
 
 # Create your views here.
 def cadastro(request):
@@ -13,18 +14,18 @@ def cadastro(request):
         confirmar_senha = request.POST.get('confirmar_senha')
         
         if senha != confirmar_senha:
-            print('erro2') #retirar print
+            messages.add_message(request, constants.ERROR, "A senha e o confirmar senha devem ser iguais")
             return redirect('/usuarios/cadastro')
         
 
         if len(senha) < 6:
-            print('erro3') #retirar print
+            messages.add_message(request, constants.ERROR, "A senha deve ter mais de 6 dígitos")
             return redirect('/usuarios/cadastro')
         
         users = User.objects.filter(username=username)
         
         if users.exists():
-            print('Erro1"=') #retirar print
+            messages.add_message(request, constants.ERROR, "Já existe um usuário com esse username")
             return redirect('/usuarios/cadastro')
         
         user = User.objects.create_user(
@@ -32,4 +33,8 @@ def cadastro(request):
             email=email,
             password=senha
         ) 
-    return HttpResponse(f'Usuario criado com sucesso.')
+    return redirect('usuarios/login')
+
+def login_view(request):
+    if request.method == "GET":
+        return render(request, 'login.html')
