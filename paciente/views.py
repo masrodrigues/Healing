7de9add0580305fcf54e645fe090bd2f655b62ba
Marco Rodrigues <84645887+masrodrigues@users.noreply.github.com>
2024-05-02 16:44:28 -0300

    feat: filtro dos medicos

--- paciente/views.py
+++ paciente/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
-from medico.models import DadosMedico
+from medico.models import DadosMedico, Especialidades
 
 # Create your views here.
 def home(request):
     if request.method == 'GET':
+        medico_filtrar = request.GET.get('medico')
+        especialidades_filtrar = request.GET.getlist('especialidades')
         medicos = DadosMedico.objects.all()
+        
+        if medico_filtrar:
+            medicos = medicos.filter(nome__icontains=medico_filtrar)
+            
+        if especialidades_filtrar:
+            medicos = medicos.filter(especialidade_id__in=especialidades_filtrar)
+        
+        especialidades = Especialidades.objects.all()
         print(medicos)
-        return render(request, 'home.html', {'medicos': medicos})
+        return render(request, 'home.html', {'medicos': medicos, 'especialidades': especialidades})
